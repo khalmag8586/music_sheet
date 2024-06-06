b2007444abcd1607ccd5d05121419afba7dd2343
khalmag8586 <khalmag8586@gmail.com>
2024-06-06 08:00:10 +0300

    Reapply .gitignore rules

--- apps/customer/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.customer'
-
-    def ready(self):
-        import apps.customer.signals
--- apps/permissions_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PermissionsApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.permissions_api'
